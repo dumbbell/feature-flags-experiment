289c2c48104a3d10791662dbf3c80acd2bc4c55b
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2018-07-11 13:28:34 +0200

    Enable feature flags clsuter-wide
    Also, try to enable all feature flags on startup.

--- src/feature_flags_worker.erl
+++ src/feature_flags_worker.erl
@@ -40,16 +40,26 @@ init([]) ->
                     {ok, v1} -> ?FEATURE_FLAGS_V1;
                     {ok, v2} -> ?FEATURE_FLAGS_V2
                 end,
-    State0 = #state{supported = Supported},
-    case is_local_node_compatible_with_remote_nodes(State0) of
+    State = #state{supported = Supported},
+    %% 1. To start, a node must support all features enabled in the cluster.
+    %% 2. It must then enable all features enabled elsewhere.
+    RemotelyEnabledPerNode = query_enabled_feature_flags_on_remote_nodes(
+                               State),
+    case are_remote_feature_flags_supported(RemotelyEnabledPerNode, State) of
         true ->
-            {_, State1} = enable_all(State0),
-            log_feature_flags(State1),
-            {ok, TRef} = timer:apply_interval(1000, gen_server, cast, [?MODULE, run]),
-            State2 = State1#state{run_timer = TRef},
-            {ok, State2};
+            ToEnable = case RemotelyEnabledPerNode of
+                           [] -> Supported;
+                           _  -> sumup_remote_feature_flags(
+                                   RemotelyEnabledPerNode)
+                       end,
+            case enable_feature_flags(ToEnable, State) of
+                {ok, State1} ->
+                    {ok, State1};
+                {Error, _State1} ->
+                    {stop, {failed_to_enable_feature_flags, Error}}
+            end;
         false ->
-            {stop, enabled_features_unsupported}
+            {stop, incompatible_with_remote_nodes}
     end.
 
 handle_call(supported, _From, #state{supported = Supported} = State) ->
@@ -61,13 +71,25 @@ handle_call(enabled, _From, #state{enabled = Enabled} = State) ->
 handle_call({is_supported, Flag}, _From, State) ->
     {reply, is_supported(Flag, State), State};
 
-handle_call(enable_all, _From, State) ->
-    {Reply, State1} = enable_all(State),
+handle_call(enable_all_feature_flags, _From, State) ->
+    {Reply, State1} = enable_all_feature_flags(State),
     {reply, Reply, State1};
 
 handle_call({enable, Flag}, _From, State) ->
-    {Reply, State1} = enable([Flag], State),
+    {Reply, State1} = enable_feature_flags([Flag], State),
     {reply, Reply, State1};
+
+handle_call({enable_locally, Flag}, _From, State) ->
+    {Reply, State1} = enable_feature_flag_locally(Flag, State),
+    {reply, Reply, State1};
+
+handle_call({query_feature_flags, supported}, _From,
+            #state{supported = Supported} = State) ->
+    {reply, Supported, State};
+
+handle_call({query_feature_flags, enabled}, _From,
+            #state{enabled = Enabled} = State) ->
+    {reply, Enabled, State};
 
 handle_call(_, _From, State) ->
     {reply, undefined, State}.
@@ -75,10 +97,6 @@ handle_call(_, _From, State) ->
 handle_cast(list, State) ->
     log_feature_flags(State),
     {noreply, State};
-
-handle_cast({try_enable, Flag}, State) ->
-    {_, State1} = enable([Flag], State),
-    {noreply, State1};
 
 handle_cast(run, State) ->
     run(State),
@@ -101,7 +119,7 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal functions.
 %% -------------------------------------------------------------------
 
-all_flags(#state{supported = Supported}) ->
+all_feature_flags(#state{supported = Supported}) ->
     Supported.
 
 is_supported(Flag, #state{supported = Supported}) ->
@@ -110,38 +128,43 @@ is_supported(Flag, #state{supported = Supported}) ->
 is_enabled(Flag, #state{enabled = Enabled}) ->
     lists:member(Flag, Enabled).
 
-enable_all(State) ->
-    AllFlags = all_flags(State),
-    enable(AllFlags, State).
+enable_all_feature_flags(State) ->
+    AllFlags = all_feature_flags(State),
+    enable_feature_flags(AllFlags, State).
 
-enable([Flag | Rest], State) ->
-    case enable_one(Flag, State) of
-        {ok, State1}    -> enable(Rest, State1);
+enable_feature_flags([Flag | Rest], State) ->
+    case enable_feature_flag(Flag, State) of
+        {ok, State1}    -> enable_feature_flags(Rest, State1);
         {Error, State1} -> {Error, State1}
     end;
-enable([], State) ->
+enable_feature_flags([], State) ->
     {ok, State}.
 
-enable_one(Flag, State) ->
+enable_feature_flag(Flag, State) ->
+    Nodes = participating_nodes(State),
+    case are_remote_nodes_compatible(Nodes, Flag, State) of
+        true -> enable_feature_flag(Nodes, Flag, State);
+        false -> {{error, {incompatible_with_remote_nodes, Flag}}, State}
+    end.
+
+enable_feature_flag([Node | Rest], Flag, State) ->
+    case gen_server:call({?MODULE, Node}, {enable_locally, Flag}) of
+        ok    -> enable_feature_flag(Rest, Flag, State);
+        Error -> {Error, State}
+    end;
+enable_feature_flag([], Flag, State) ->
+    enable_feature_flag_locally(Flag, State).
+
+enable_feature_flag_locally(Flag, State) ->
     case is_enabled(Flag, State) of
         true ->
             {ok, State};
         false ->
             case is_supported(Flag, State) of
-                true  -> try_to_enable(Flag, State);
+                true  -> do_enable(Flag, State);
                 false -> {{error, {unsupported, Flag}}, State}
             end
     end.
-
-try_to_enable(Flag, State) ->
-    case are_remote_nodes_compatible(Flag, State) of
-        true  -> do_enable(Flag, State);
-        false -> {{error, incomptible_with_cluster}, State}
-    end.
-
-are_remote_nodes_compatible(Flag, State) ->
-    Nodes = nodes(),
-    are_remote_nodes_compatible(Nodes, Flag, State).
 
 are_remote_nodes_compatible(Nodes, Flag, _State) ->
     Result = [{Node,
@@ -156,30 +179,21 @@ are_remote_nodes_compatible(Nodes, Flag, _State) ->
                       || {Node, Supported} <- Result]]),
     lists:all(fun({_, I}) -> I =:= true orelse I =:= down end, Result).
 
-is_local_node_compatible_with_remote_nodes(State) ->
-    Nodes = nodes(),
-    is_local_node_compatible_with_remote_nodes(Nodes, State).
-
-is_local_node_compatible_with_remote_nodes(Nodes, #state{supported = Supported}) ->
-    Result = [{Node,
-               try
-                   RemotelyEnabled = gen_server:call({?MODULE, Node}, enabled),
-                   RemotelyEnabled -- Supported
-               catch
-                   exit:{noproc, _} -> down
-               end}
-              || Node <- Nodes],
-    io:format("Unsupported feature flags enabled on remote nodes:~n~s~n",
-              [[io_lib:format("  ~s: ~p~n", [Node, Unsupported])
-                || {Node, Unsupported} <- Result, Unsupported =/= []]]),
-    lists:all(fun({_, I}) -> I =:= [] orelse I =:= down end, Result).
-
 do_enable(Flag, #state{enabled = Enabled} = State) ->
-    Enabled1 = Enabled ++ [Flag],
-    State1 = State#state{enabled = Enabled1},
-    log_feature_flags(State1),
-    [gen_server:cast({?MODULE, Node}, {try_enable, Flag}) || Node <- nodes()],
-    {ok, State1}.
+    case try_to_enable_deps(Flag, State) of
+        {ok, State1} ->
+            Enabled1 = Enabled ++ [Flag],
+            State2 = State1#state{enabled = Enabled1},
+            log_feature_flags(State2),
+            {ok, State2};
+        {Error, State1} ->
+            {Error, State1}
+    end.
+
+try_to_enable_deps(v1, State) ->
+    {ok, State};
+try_to_enable_deps(v2, State) ->
+    enable_feature_flag_locally(v1, State).
 
 log_feature_flags(#state{supported = Supported} = State) ->
     log_feature_flags1(Supported, State, []).
@@ -198,3 +212,62 @@ run(State) ->
         true  -> io:format("Running \033[1;32mV2\033[0m code...~n", []);
         false -> io:format("Running \033[1;33mV1\033[0m code...~n", [])
     end.
+
+%% -------------------------------------------------------------------
+
+%query_supported_feature_flags_on_remote_nodes(#state{} = State) ->
+%    Nodes = participating_nodes(State),
+%    query_supported_feature_flags_on_nodes(Nodes).
+%
+%query_supported_feature_flags_on_nodes(Nodes) ->
+%    query_feature_flags_on_nodes(Nodes, supported).
+
+query_enabled_feature_flags_on_remote_nodes(#state{} = State) ->
+    Nodes = participating_nodes(State),
+    query_enabled_feature_flags_on_nodes(Nodes).
+
+query_enabled_feature_flags_on_nodes(Nodes) ->
+    query_feature_flags_on_nodes(Nodes, enabled).
+
+query_feature_flags_on_nodes(Nodes, Status) ->
+    [{Node,
+      try
+          gen_server:call({?MODULE, Node}, {query_feature_flags, Status})
+      catch
+          exit:{noproc, _} -> down
+      end}
+     || Node <- Nodes].
+
+are_remote_feature_flags_supported(NodesAndFeatureFlags, State) ->
+    Result = [case FeatureFlags of
+                  down -> true; % TODO: How should we treat a remote node which is down?
+                  _    -> are_feature_flags_supported(FeatureFlags, State)
+              end
+              || {_, FeatureFlags} <- NodesAndFeatureFlags],
+    all_true(Result).
+
+are_feature_flags_supported(FeatureFlags, State) ->
+    Result = [is_feature_flag_supported(FeatureFlag, State) || FeatureFlag <- FeatureFlags],
+    all_true(Result).
+
+is_feature_flag_supported(FeatureFlag, #state{supported = Supported}) ->
+    lists:member(FeatureFlag, Supported).
+
+all_true(Result) ->
+    lists:all(fun(I) -> I end, Result).
+
+sumup_remote_feature_flags(NodesAndFeatureFlags) ->
+    sumup_remote_feature_flags(NodesAndFeatureFlags, []).
+
+sumup_remote_feature_flags([{_, FeatureFlags} | Rest], Result)
+  when is_list(FeatureFlags) ->
+    Result1 = lists:merge(Result, FeatureFlags),
+    sumup_remote_feature_flags(Rest, Result1);
+sumup_remote_feature_flags([{_, down} | Rest], Result) ->
+    % TODO: What to do when a remote node doesn't respond?
+    sumup_remote_feature_flags(Rest, Result);
+sumup_remote_feature_flags([], Result) ->
+    Result.
+
+participating_nodes(_State) ->
+    nodes().
